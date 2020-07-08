e66905e3be3cae1d352baa90899f6d42bcfcede1
Khanahs <khanhvuong247@gmail.com>
2020-07-08 17:02:14 +0700

    css sang scss

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,8 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="{{URL::asset('css/homePagePHP.css')}}">
-    <link rel="stylesheet" href="{{URL::asset('FontAwesome/css/all.min.css')}}">
+{{--    <link rel="stylesheet" href="{{asset('sass/home.scss')}}">--}}
+{{--    <link rel="stylesheet" href="{{asset('FontAwesome/css/all.min.css')}}">--}}
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
