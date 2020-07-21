7cac0b16d459a19d6baef016aca8ba77493ea133
Hạnh <dohonghanh2101@gmail.com>
2020-07-21 16:47:15 +0700

    Cart 21/07

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+
+class CartController extends BaseController
+{
+    public function showCart(){
+        return view('Cart/cart');
+    }
+}
