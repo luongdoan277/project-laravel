e9702e1af1401784196cae546ab518bed07f9f2f
Hạnh <dohonghanh2101@gmail.com>
2020-08-03 16:09:53 +0700

    commit

--- public/js/qty.js
+++ public/js/qty.js
@@ -1,30 +1,18 @@
 
-function plus() {
-    var t = document.getElementById('textbox').value;
-    document.getElementById('textbox').value = parseInt(t) + 1;
-}
-
-plus();
-
-function minus() {
-    var t = document.getElementById('textbox').value;
-    if(parseInt(t) > 1){
-        document.getElementById('textbox').value = parseInt(t) - 1;
-    }
-}
-minus();
-
-$(function () {
-    let $elm_value = $(".value");
-    $('.increase').click(function(){
-        let $value_increase = parseInt($elm_value.text());
-        if($value_increase <= 1){
-            return false;
+var proQty = $('.pro-qty');
+proQty.prepend('<span class = "dec qtybtn">-</span>')
+proQty.append('<span class="inc qtybtn">+</span>')
+proQty.on('click','.qtybtn',function(){
+    var $button = $(this);
+    var oldValue =$button.parent().find('input').val();
+    if($button.hasClass('inc')){
+        var newVal = parseFloat(oldValue) + 1;
+    }else{
+        if(oldValue > 0){
+            var newVal = parseFloat(oldValue)-1;
+        }else{
+            newVal = 0;
         }
-        $elm_value.text($value_increase - 1);
-    })
-    $('.reduction').click(function(){
-        let $value_increase = parseInt($elm_value.text());
-        $elm_value.text($value_increase + 1);
-    })
+    }
+    $button.parent().find('input').val(newVal);
 });
--- resources/views/Cart/cart.blade.php
+++ resources/views/Cart/cart.blade.php
@@ -7,7 +7,10 @@
 </head>
 <body>
 @yield('content')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>--}}
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" ></script>
 <script src="{{asset('js/qty.js')}}"></script>
 <script src="{{asset('js/menu-hamburgerProduct.js')}}"></script>
 </body>
